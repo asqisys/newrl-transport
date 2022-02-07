af1b613550aeeb7aef7d1fc41e89d7cf3ae83897
Lakshay Dahiya <lakshay@asqi.in>
2022-02-07 12:09:20 +0530

    Updated bootstrap list

--- src/data/const.js
+++ src/data/const.js
@@ -1,5 +1,5 @@
 const bootstrapList = [
-    '/ip4/18.140.71.178/tcp/52724/p2p/QmPdj9wRForzrDe38nToDqBnkbYEvcXdqjq8TPdtdhWqGz'
+    '/ip4/18.140.71.178/tcp/52724/p2p/QmadKJaVCnkppgFWaVzaKz2fMDaYz1tiKifepyUvdjHtDC'
 ]
 const IDENTITY_FILE_PATH = "./src/data/identity.json"
 
