e976d2ba0b0fb9352049d2c2c466d0c06bd2ca74
Lakshay Dahiya <lakshay@asqi.in>
2022-02-07 21:45:11 +0530

    Bug Fix-4

--- src/data/const.js
+++ src/data/const.js
@@ -1,5 +1,6 @@
 const bootstrapList = [
-    '/ip4/18.140.71.178/tcp/52724/p2p/QmadKJaVCnkppgFWaVzaKz2fMDaYz1tiKifepyUvdjHtDC'
+    // '/ip4/18.140.71.178/tcp/52724/p2p/QmadKJaVCnkppgFWaVzaKz2fMDaYz1tiKifepyUvdjHtDC'
+    '/ip4/13.38.125.254/tcp/52724/p2p/QmQQFAYzQWJW3vf84azLWyqLuWxciqGEjPNSfkmcmKXkCy'
 ]
 const IDENTITY_FILE_PATH = "./src/data/identity.json"
 
