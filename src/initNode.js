47639dbc4a0d1890eb42ea85fafac89ad89c4c2b
Lakshay Dahiya <lakshay@asqi.in>
2022-02-08 17:40:13 +0530

    Bug Fix-4

--- src/initNode.js
+++ src/initNode.js
@@ -4,7 +4,7 @@ const {registerPeer} = require("./discovery");
 const process = require("process");
 const {listener, internalListener, peerListener} = require("./listener");
 const {generatePeerID} = require("./libp2p/baseNode");
-const {printAddress} = require("./utility/utility");
+const {printAddress, createPath} = require("./utility/utility");
 const {sendPeerList} = require("./dialer");
 let extIP = require('ext-ip')();
 
@@ -20,8 +20,8 @@ function attachLibp2pEvents(node) {
     node.connectionManager.on('peer:connect', (connection) => {
         console.log('Connection established to:', connection.remotePeer.toB58String())
         console.log('Sending peer list to :', connection.remotePeer.toB58String())
-        node.peerStore.addressBook.add(connection.remotePeer, connection.remoteAddr)
-        sendPeerList(node,connection.remotePeer);
+        // node.peerStore.addressBook.add(connection.remotePeer, connection.remoteAddr)
+        sendPeerList(node,connection.remoteAddr.toJSON() +'/'+connection.remotePeer.toB58String());
     });
 
     // node.peerStore.on('peer', (peerId) => {
